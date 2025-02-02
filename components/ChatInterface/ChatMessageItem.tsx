dd7dd64599f46404fedebca80296e66152c4edd7
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-02-03 01:41:57 +0200

    restyled the chatting msgs items

--- components/ChatInterface/ChatMessageItem.tsx
+++ components/ChatInterface/ChatMessageItem.tsx
@@ -2,16 +2,20 @@ import { memo } from "react";
 import { ChatMessage } from "@/lib/dexie";
 
 const ChatMessageItem = memo(({ message }: { message: ChatMessage }) => {
+  const isUser = message.role === "user";
+
   return (
-    <div
-      className={`p-4 rounded ${
-        message.role === "user" ? "bg-neutral-900" : "bg-neutral-800"
-      }`}
-    >
-      <div className="font-bold">
-        {message.role === "user" ? "You" : "Assistant"}:
+    <div className={`flex ${isUser ? "justify-end" : "justify-start"} my-2`}>
+      <div
+        className={`max-w-[75%] p-3 rounded-lg text-white shadow-md ${
+          isUser ? "bg-neutral-900" : "bg-neutral-700"
+        } ${isUser ? "rounded-br-none" : "rounded-bl-none"}`}
+      >
+        
+        <div className=" text-sm font-medium leading-relaxed">
+          {message.content.replace("</think>", "").replace("<think>", "")}
+        </div>
       </div>
-      <div className="whitespace-pre-wrap">{message.content}</div>
     </div>
   );
 });
