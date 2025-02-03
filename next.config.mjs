3f6e38c4c4ac4ee682afc959bb8bd52accb07045
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-02-04 01:25:46 +0200

    separated the next-pwa configuration from the Next.js configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,21 @@
 /** @type {import('next').NextConfig} */
 import withPWA from 'next-pwa';
 
-const nextConfig = withPWA({
+const pwaConfig = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development', // Disable PWA in development
   register: true,
   skipWaiting: true,
+});
+
+const nextConfig = {
+  ...pwaConfig, // Spread the PWA configuration here
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-});
+};
 
 export default nextConfig;
