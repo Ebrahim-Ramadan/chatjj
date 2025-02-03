7be9dfa4eb1c02de91052ac693b551277396a86c
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-02-03 06:57:59 +0200

    i think i fixed clerk shitty states

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
   const userChats = await getUserChats(session.userId);
   console.log('userChats', userChats);
   return (
-    <ClerkProvider>
+    <ClerkProvider dynamic>
     <html lang="en">
       <head>
       <script
